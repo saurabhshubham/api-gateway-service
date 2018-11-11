4e655e9ad3723736f2e18d752d160969d79e080a
unknown <shubhamsaurav03@gmail.com>
2018-11-11 12:26:11 +0530

    first commit

---
+++ src/main/java/com/file/gateway/apigatewayservice/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.file.gateway.apigatewayservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .authorizeRequests().antMatchers("*").hasRole("USER")
+                .anyRequest().authenticated()
+                .and().httpBasic();
+        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("username").password("password").roles("USER");
+    }
+}
---
+++ src/main/java/com/file/gateway/apigatewayservice/filter/LoggingFilter.java
@@ -0,0 +1,51 @@
+package com.file.gateway.apigatewayservice.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class LoggingFilter extends ZuulFilter {
+
+	private  final Logger LOG = LoggerFactory.getLogger(this.getClass());
+	@Override
+	public Object run() throws ZuulException {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+
+		System.out.println(
+				"Request Method : " + request.getMethod() + " Request URL : " + request.getRequestURL().toString());
+		
+		LOG.debug(
+				"Request Method : " + request.getMethod() + " Request URL : " + request.getRequestURL().toString());
+
+
+		
+		return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		
+		return 1;
+	}
+
+	@Override
+	public String filterType() {
+	
+		return "pre";
+	}
+
+}
